d7d38f5b5fed9449ee5b044c183085936af427e7
Valentina Cambiaso <valecambiaso@gmail.com>
2020-09-14 18:25:54 -0300

    cuarto commit

--- Persona.java
+++ Persona.java
@@ -2,9 +2,11 @@ package tdp;
 
 public class Persona {
 	private String name;
+	private int dni;
 	
-	public Persona(String name) {
+	public Persona(String name,int dni) {
 		this.name = name;
+		this.dni = dni;
 	}
 	
 	public String getName() {
@@ -13,5 +15,13 @@ public class Persona {
 	
 	public void setName(String name) {
 		this.name = name;
+	}
+
+	public int getDni() {
+		return this.dni;
+	}
+	
+	public void setDni(int dni) {
+		this.dni = dni;
 	}
 }
---
+++ Prueba2.java
@@ -0,0 +1,9 @@
+package tdp;
+
+public class Prueba2 {
+	
+	public static void main(String[] args) {
+		Persona p = new Persona("Fulanito");
+		System.out.println("nombre: " + p.getName());
+	}
+}
